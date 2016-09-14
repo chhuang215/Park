d397c8643b947199e2f45dc04e92e57df388620b
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-09-13 22:25:04 -0600

    fixed multiple markers, added session(no use yet), added listener for navigation(no use)

--- client/parkMap.js
+++ client/parkMap.js
@@ -2,7 +2,17 @@ import { Template } from 'meteor/templating';
 
 import { GoogleMaps } from 'meteor/dburles:google-maps';
 import {Geolocation} from 'meteor/mdg:geolocation';
+import { Session } from 'meteor/session';
 import './parkMap.html';
+
+var parkingSpot = [
+    // Test Marker: Northland Walmart parking
+    {position:{lat:51.0982289, lng:-114.1450605}, info:"Hello fker, This is ghetto Walmart."},
+    {position:{lat:51.13661, lng:-114.160626}, info:"You sick fk, This is poor people SuperStore."}
+//    {lat:51.0440916, lng:-114.1900152},
+//    {lat:51.1254236, lng:-114.1563701},
+//    {lat:51.0913963, lng:-114.0479861},
+];
 
 Template.parkMap.helpers({
   mapOptions() {
@@ -30,58 +40,60 @@ Template.parkMap.helpers({
   },
 });
 
+Template.parkMap.events({
+  'click .btnNavTo'(event) {
+    console.log(event.currentTarget.id);
+
+  },
+});
+
+Template.parkMap.onRendered(function(){
+
+});
+
+var openedInfoWindow = null;
 
 Template.parkMap.onCreated(function (){
   var self = this;
   GoogleMaps.ready('parkMap', function(map) {
    // TEST: Add a marker to the map once it's ready
 
-    var positions = [
-        // Test Marker: Northland Walmart parking
-        {lat:51.0982289, lng:-114.1450605}
-  //    {lat:51.0440916, lng:-114.1900152},
-  //    {lat:51.1254236, lng:-114.1563701},
-  //    {lat:51.0913963, lng:-114.0479861},
-   ];
+
 
    // Put the markers on
-   for (var i = 0; i < positions.length; i++) {
-     // Initialize new marker
-     var marker = new google.maps.Marker({
-       position: positions[i],
-       map: map.instance,
-       label: (i+1).toString(),
-     });
+   for (var i = 0; i < parkingSpot.length; i++) {
 
-     var contentString = '<div id=p'+i+'>Hello mofo <button>Click here to fking navigate</button></div>'
-    //  var contentString = '<div id="content">'+
-    //         '<div id="siteNotice">'+
-    //         '</div>'+
-    //         '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    //         '<div id="bodyContent">'+
-    //         '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    //         'sandstone rock formation in the southern part of the '+
-    //         'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    //         'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    //         '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    //         'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    //         'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    //         'Aboriginal people of the area. It has many springs, waterholes, '+
-    //         'rock caves and ancient paintings. Uluru is listed as a World '+
-    //         'Heritage Site.</p>'+
-    //         '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    //         'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    //         '(last visited June 22, 2009).</p>'+
-    //         '</div>'+
-    //         '</div>';
 
      var locationInfoWindow = new google.maps.InfoWindow({
-        content: contentString
+        content: '<div id=p'+i+'><p>'+ parkingSpot[i].info +'</p> <br /> <button class="btn btn-warning btnNavTo" id="'+i+'">Click here to fking navigate</button></div>'
       });
+      // Add event listener for infowindow
+      // google.maps.event.addListener(locationInfoWindow, 'domready', function() {
+      //     console.log("LISTENER: " + "btnNavTo"+i) ;
+      //     document.getElementById("btnNavTo"+i).addListener("click", function(e) {
+      //       //  e.stop();
+      //         console.log("hi! " + i);
+      //     });
+      // });
 
-     marker.addListener('click', function() {
-       locationInfoWindow.open(map, marker);
+
+      // Initialize new marker
+     var marker = new google.maps.Marker({
+       position: parkingSpot[i].position,
+       map: map.instance,
+       label: (i+1).toString(),
+       infowindow: locationInfoWindow
      });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        // Close any existing infowindow
+
+        if(openedInfoWindow) openedInfoWindow.close();
+
+        // Open infowindow when marker is clicked
+        openedInfoWindow = this.infowindow;
+        this.infowindow.open(map, this);
+    });
    }
 
    var currMarker;
@@ -103,12 +115,12 @@ Template.parkMap.onCreated(function (){
      else {
         currMarker.setPosition(latLng);
     }
-    var centerControlDiv = document.createElement('div');
-    var centerControl = new CenterControl(centerControlDiv, map, chicago);
+    // var centerControlDiv = document.createElement('div');
+    // var centerControl = new CenterControl(centerControlDiv, map, chicago);
 
     // Center and zoom the map view onto the current position.
-     map.instance.setCenter(marker.getPosition());
-     map.instance.setZoom(15);
+     map.instance.setCenter(currMarker.getPosition());
+     map.instance.setZoom(8);
    });
   });
 });
