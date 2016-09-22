be7aa3e406e151cfa153ae55a47e13c68348d59c
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-09-22 00:43:31 -0600

    parking spots only display within radius; able to navigate from parking spot to destination with walking mode;

--- client/parkMap.js
+++ client/parkMap.js
@@ -6,12 +6,16 @@ import { Session } from 'meteor/session';
 import { ParkingSpot } from '/lib/collections/ParkingSpot.js';
 import './parkMap.html';
 
+const DEFAULT_RADIUS = 500;
+
 var openedInfoWindow = null;
 var currentDestinationMarker = null;
 var mouseup = false;
 var drag = false;
 var radiusCircle = null;
 var currMarker = null;
+var allParkingSpots = [];
+var visibleMarker = [];
 Template.parkMap.helpers({
   mapOptions() {
     // check if maps API has loaded
@@ -40,39 +44,33 @@ Template.parkMap.helpers({
 
 Template.parkMap.events({
   'click .btnNavToParkingSpot'(event) {
-    //console.log();
+
     if(openedInfoWindow) openedInfoWindow.close();
     let spot = ParkingSpot.findOne({_id: event.currentTarget.id});
 
-    let map = GoogleMaps.maps.parkMap.instance;
     let fromLocation = Geolocation.latLng();
     let toLocation = spot.position;
 
-    let directionsService = new google.maps.DirectionsService();
-    let directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
-    let request = {
-      origin: fromLocation,
-      destination: toLocation,
-      travelMode: 'DRIVING',
-      drivingOptions: {
-        departureTime: new Date(Date.now()),
-        trafficModel: 'optimistic',
-      },
-      provideRouteAlternatives: true
-    }
-    directionsService.route(request, function(result, status) {
-      if (status == 'OK') {
-        directionsDisplay.setDirections(result);
-      }
-    });
-  },
-
-  'click .btnNavToDestination'(event){
+    driveToDestination(fromLocation,toLocation);
 
   },
+  'click .btnNavToSpotAndWalk'(event){
+    if(openedInfoWindow) openedInfoWindow.close();
+    let spot = ParkingSpot.findOne({_id: event.currentTarget.id});
+    let fromLocation = Geolocation.latLng();
+    let toLocation = spot.position;
+
+    driveToDestination(fromLocation,toLocation);
+
+    if(!currentDestinationMarker) return;
+    fromLocation = toLocation;
+
+    toLocation = currentDestinationMarker.getPosition();
 
 
+    walkToDestination(fromLocation,toLocation);
+
+  }
 });
 
 Template.parkMap.onCreated(function (){
@@ -83,16 +81,14 @@ Template.parkMap.onCreated(function (){
 
   GoogleMaps.ready('parkMap', function(map) {
 
+    let mapInstance = map.instance;
+
     // Close any window if user click on anywhere on the map
-    map.instance.addListener('click', function(e) {
+    mapInstance.addListener('click', function(e) {
       if(openedInfoWindow) openedInfoWindow.close();
     });
 
-    // map.instance.addListener('rightclick', function(e) {
-    //   setNewDestination(map, e.latLng);
-    // });
-
-    map.instance.addListener('mousedown', function(e) {
+    mapInstance.addListener('mousedown', function(e) {
       mousedUp = false;
       // If the user long press a location, adds a new marker as new destination
       setTimeout(function(){
@@ -103,16 +99,16 @@ Template.parkMap.onCreated(function (){
 
     });
 
-    map.instance.addListener('mouseup', function(event){
+    mapInstance.addListener('mouseup', function(event){
       mousedUp = true;
       drag = false;
     });
 
-    map.instance.addListener('drag', function(event){
+    mapInstance.addListener('drag', function(event){
       drag = true;
     });
 
-    map.instance.addListener('dragend', function(event){
+    mapInstance.addListener('dragend', function(event){
       drag = false;
     });
 
@@ -120,34 +116,42 @@ Template.parkMap.onCreated(function (){
    let parkingSpot = ParkingSpot.find().fetch();
    for (var i = 0; i < parkingSpot.length; i++) {
 
-     let contentInfo = '<div id=p'+i+'><p>'+ parkingSpot[i].info +'</p> <br /> <button class="btn btn-warning btnNavToParkingSpot" id="'+parkingSpot[i]._id+'">Click here to fking navigate</button></div>';
-     var locationInfoWindow = new google.maps.InfoWindow({
+     let contentInfo =
+      '<div id=p'+i+'>'+
+        '<p>'+ parkingSpot[i].info +'</p>'+
+        '<br />'+
+        '<button class="btn btn-sm btn-warning btnNavToParkingSpot" id="'+parkingSpot[i]._id+'">Click here to fking navigate</button>' +
+        '<br />'+
+        '<button class="btn btn-sm btn-success btnNavToSpotAndWalk" id="'+parkingSpot[i]._id+'">Click here to park and walk</button>'+
+      '</div>' ;
+     let locationInfoWindow = new google.maps.InfoWindow({
         content: contentInfo
       });
 
       // Initialize new markers
-     var marker = new google.maps.Marker({
+     let marker = new google.maps.Marker({
        position: parkingSpot[i].position,
-       map: map.instance,
        label: (i+1).toString(),
-       infowindow: locationInfoWindow
+       infowindow: locationInfoWindow,
+       id: parkingSpot[i]._id
      });
 
-    google.maps.event.addListener(marker, 'click', function () {
+    marker.addListener('click', function () {
         // Close any existing infowindow
         if(!currentDestinationMarker){
           if(openedInfoWindow) openedInfoWindow.close();
-
-          // Open infowindow when marker is clicked
-          openedInfoWindow = this.infowindow;
-          this.infowindow.open(map, this);
         }
+        // Open infowindow when marker is clicked
+        openedInfoWindow = this.infowindow;
+        this.infowindow.open(map, this);
     });
+    allParkingSpots.push(marker);
+    console.log(allParkingSpots);
    }
 
   let latLng = null;
 
-  radiusCircle = createCircleRadius(map.instance, 500);
+  radiusCircle = createCircleRadius(map.instance, DEFAULT_RADIUS);
 
   // Create and move the marker when latLng changes.
   self.autorun(function() {
@@ -165,19 +169,17 @@ Template.parkMap.onCreated(function (){
 
        // Center and zoom the map view onto the current position.
       map.instance.setZoom(14);
-       map.instance.setCenter(currMarker.getPosition());
+      map.instance.setCenter(currMarker.getPosition());
 
      }
      else {
        currMarker.setPosition(latLng);
      }
+     findNearSpots(currMarker.getPosition(), map);
+     // Display radius if the radius if there is no destination set
+     if(!currentDestinationMarker)
+        radiusCircle.bindTo('center', currMarker, 'position');
 
-    // Display radius if the radius if there is no destination set
-    if(!currentDestinationMarker)
-      radiusCircle.bindTo('center', currMarker, 'position');
-
-    // var centerControlDiv = document.createElement('div');
-    // var centerControl = new CenterControl(centerControlDiv, map, chicago);
    });
   });
 });
@@ -190,8 +192,8 @@ Template.parkMap.onRendered(function() {
 /**
  * Generate a new circle overlay with default radius=500
  */
-function createCircleRadius(map, radius = 500, color = '#7BB2CA'){
-  //if(!radius) radius = 500;
+function createCircleRadius(map, radius = DEFAULT_RADIUS, color = '#7BB2CA'){
+
   let circle = new google.maps.Circle({
     map: map,
     radius: radius,    // 500 km default
@@ -200,7 +202,7 @@ function createCircleRadius(map, radius = 500, color = '#7BB2CA'){
     fillOpacity: 0.2,
     clickable: false,
   //  center: currMarker.position
-  });;
+  });
   return circle;
 }
 
@@ -208,51 +210,37 @@ function createCircleRadius(map, radius = 500, color = '#7BB2CA'){
  * Add marker on the map to set a new destination
  */
 function setNewDestination(map, latLng){
+
+
+
   if(currentDestinationMarker){
-    currentDestinationMarker.setPosition(latLng);;
+    currentDestinationMarker.setPosition(latLng);
   }
   else{
-
-    let contentInfo = '<div> <button class="btn btn-warning btnNavToDestination">Click here to fking navigate</button></div>';
-
-    let destinationInfoWindow = new google.maps.InfoWindow({
-       content: contentInfo
-    });
 
     currentDestinationMarker = new google.maps.Marker({
       position:latLng,
       draggable: true,
       label: 'D',
       map: map.instance,
-      infowindow: destinationInfoWindow
     });
-
 
     // Display radius
     radiusCircle.bindTo('center', currentDestinationMarker, 'position');
+
     //radiusCircle.setMap(map.instance);
-
-    google.maps.event.addListener(currentDestinationMarker, 'click', function () {
-        // Close any existing infowindow
-
-        if(openedInfoWindow) openedInfoWindow.close();
-
-          // Open infowindow when marker is clicked
-        openedInfoWindow = this.infowindow;
-        this.infowindow.open(map, this);
-
-    });
 
     google.maps.event.addListener(currentDestinationMarker,'mousedown', function(e) {
       mousedUp = false;
       setTimeout(function(){
           if(mousedUp === false && !drag){
+
             currentDestinationMarker.setMap(null);
             currentDestinationMarker=null;
             radiusCircle.bindTo('center', currMarker, 'position');
+            findNearSpots(currMarker.getPosition(), map);
           }
       }, 500);
-
     });
 
     google.maps.event.addListener(currentDestinationMarker,'mouseup', function(event){
@@ -266,6 +254,107 @@ function setNewDestination(map, latLng){
 
     google.maps.event.addListener(currentDestinationMarker,'dragend', function(event){
       drag = false;
+      findNearSpots(currentDestinationMarker.getPosition(), map);
     });
   }
+  findNearSpots(latLng, map);
+
+}
+
+/**
+ * Find near parking spot with provided center location
+ */
+function findNearSpots(latLng, map){
+  let lat = latLng.lat();
+  let lng = latLng.lng();
+
+  let nearSpots = ParkingSpot.find({
+    loc: {
+      $near: {
+        $geometry: {
+          type: "Point" ,
+          coordinates: [ lng , lat ]
+        },
+        $maxDistance:DEFAULT_RADIUS
+      }
+    }
+  }).fetch();
+
+  // Clear visible markers if nearby have no parking spot
+  if(nearSpots.length === 0){
+     for(var i = 0; i < visibleMarker.length;i++){
+        visibleMarker[i].setMap(null);
+     }
+     visibleMarker = [];
+     return;
+  }
+
+  // Display near parking spot markers
+  for (var i = 0; i < nearSpots.length; i++) {
+    let nearSpot = nearSpots[i];
+
+    for(var j = 0; j < allParkingSpots.length;j++){
+      let spot = allParkingSpots[j];
+      // If spot found, display it
+      if(spot.id == nearSpot._id){
+        spot.setMap(map.instance);
+        visibleMarker.push(spot);
+      }
+      // Else make it invisible if it was visible
+      else {
+        if(spot.map) spot.setMap(null);
+      }
+    }
+  }
+}
+
+function driveToDestination(fromLocation, toLocation){
+
+  let mapInstance = GoogleMaps.maps.parkMap.instance;
+  let directionsService = new google.maps.DirectionsService();
+  let directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(mapInstance);
+  directionsDisplay.setOptions( { suppressMarkers: true } );
+  let request = {
+    origin: fromLocation,
+    destination: toLocation,
+    travelMode: 'DRIVING',
+    drivingOptions: {
+      departureTime: new Date(Date.now()),
+      trafficModel: 'optimistic',
+    },
+    provideRouteAlternatives: true
+  }
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
+function walkToDestination(fromLocation, toLocation){
+  let mapInstance = GoogleMaps.maps.parkMap.instance;
+  let directionsService = new google.maps.DirectionsService();
+  let directionsDisplay = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      strokeColor: "green",
+    }
+  });
+  directionsDisplay.setMap(mapInstance);
+  directionsDisplay.setOptions( { suppressMarkers: true } );
+  let request = {
+    origin: fromLocation,
+    destination: toLocation,
+    travelMode: 'WALKING',
+    // drivingOptions: {
+    //   departureTime: new Date(Date.now()),
+    //   trafficModel: 'optimistic',
+    // },
+    //provideRouteAlternatives: true
+  }
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
 }
--- server/main.js
+++ server/main.js
@@ -4,8 +4,28 @@ import {ParkingSpot} from '/lib/collections/ParkingSpot.js';
 
 if(ParkingSpot.find().count() === 0){
   let spots = [
-    {position:{lat:51.0982289, lng:-114.1450605}, info:"Hello fker, This is ghetto Walmart."},
-    {position:{lat:51.13661, lng:-114.160626}, info:"You sick fk, This is poor people SuperStore."}
+    {
+      "loc": {
+        "type" : "Point",
+        "coordinates" : [
+          -114.1450605,
+          51.0982289
+
+        ]
+      },
+      "position":{lat:51.0982289, lng:-114.1450605},
+      "info":"Hello fker, This is ghetto Walmart."
+    },
+    {
+      "loc": {
+        "type" : "Point",
+        "coordinates" : [
+          -114.160626,51.13661
+        ]
+      },
+      "position":{lat:51.13661, lng:-114.160626},
+      "info":"You sick fk, This is poor people SuperStore."
+    }
   ];
 
   _.each(spots, function(spot) {
