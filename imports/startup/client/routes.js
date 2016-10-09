22ab213a2838b0b0ece1a0c5248ee117e72dce21
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-10-09 03:14:31 -0600

    added admin page for future modification to datas

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,8 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import {FlowRouter} from 'meteor/kadira:flow-router';
 import '/imports/ui/layouts/layout.js';
 import '/imports/ui/pages/mainPage.js';
+import '/imports/ui/pages/adminPage.js';
 FlowRouter.route('/', {
     action: function(params) {
         BlazeLayout.render("layout", {content: "mainPage"});
+    }
+});
+
+FlowRouter.route('/admin', {
+    action: function(params) {
+        BlazeLayout.render("layout", {content: "adminPage"});
     }
 });
---
+++ imports/ui/pages/adminPage.js
@@ -0,0 +1,1 @@
+import './adminPage.html';
