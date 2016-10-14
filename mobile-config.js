d411b1b773421c186cf1fa6772ea8db1a7728603
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2016-10-14 05:09:27 -0600

    mobile configuration

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  id: 'com.calgarypark',
+  name: 'calgarypark',
+  description: 'Find parking',
+  author: 'Chih-Hsuan Huang',
+//  email: 'contact@example.com',
+  //website: 'http://example.com'
+});
+
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
